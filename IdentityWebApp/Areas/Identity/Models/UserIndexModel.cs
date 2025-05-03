b8c33ecdc4aa81a87d1fc32a107ab6f1413ee411
Anton Sukhov <RealComrade2011@yandex.ru>
2025-05-03 15:15:16 +0300

    Сгенерированы страницы Index и _StatusMessage с помощью Net CLI

---
+++ IdentityWebApp/Areas/Identity/Models/UserIndexModel.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityWebApp.Areas.Identity.Models;
+
+/// <summary>
+/// Модель профиля пользователя системы.
+/// </summary>
+public class UserIndexModel
+{
+    #region Свойства
+
+    /// <summary>
+    /// Получает или задаёт имя пользователя системы.
+    /// </summary>
+    [Display(Name = "Имя")]
+    public required string Username { get; set; }
+
+    /// <summary>
+    /// Получает или задаёт номер телефона пользователя.
+    /// </summary>
+    [Phone]
+    [Display(Name = "Номер телефона")]
+    public string? PhoneNumber { get; set; }
+
+     #endregion
+}
---
+++ IdentityWebApp/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,93 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using IdentityWebApp.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityWebApp.Areas.Identity.Pages.Account.Manage
+{
+    public class IndexModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public IndexModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [BindProperty]
+        public Models.UserIndexModel Input { get; set; }
+
+        private async Task LoadAsync(ApplicationUser user)
+        {
+            var userName = await _userManager.GetUserNameAsync(user);
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+
+            Input = new Models.UserIndexModel
+            {
+                Username = userName,
+                PhoneNumber = phoneNumber
+            };
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Не удается загрузить пользователя c идентификатором '{_userManager.GetUserId(User)}'.");
+            }
+
+            await LoadAsync(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Не удается загрузить пользователя c идентификатором '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(user);
+                return Page();
+            }
+
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            if (Input.PhoneNumber != phoneNumber)
+            {
+                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+                if (!setPhoneResult.Succeeded)
+                {
+                    StatusMessage = "Непредвиденная ошибка при попытке установить номер телефона.";
+                    return RedirectToPage();
+                }
+            }
+
+            await _signInManager.RefreshSignInAsync(user);
+            StatusMessage = "Ваш профиль был обновлен.";
+            return RedirectToPage();
+        }
+    }
+}
